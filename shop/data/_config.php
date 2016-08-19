7118fe0c03a7cfaafa12df96beaaf76551b6998b
sharf <373455742@qq.com>
2016-08-19 16:04:06 +0800

    init

--- shop/data/_config.php
+++ shop/data/_config.php
@@ -2,7 +2,7 @@
 $db_config['host']     = '127.0.0.1';      //数据库主机
 $db_config['user']     = 'root';      //数据库用户名	
 $db_config['pwd']      = 'root';  //数据库用户密码	
-$db_config['name']     = 'kuaicai';      //数据库名
+$db_config['name']     = 'kuaicai';      // 数据库名
 $db_config['port']     = '3306';      //端口		
 $db_config['prefix']   = 'ecm_'; //表名前缀
 $db_config['language'] = 'utf8'; //数据库字符集 gbk,latin1,utf8..
