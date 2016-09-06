43877547a3df07f48c984b30f66dc8efc4e53924
sharf <373455742@qq.com>
2016-09-06 17:10:09 +0800

    modify img src

--- uc_client/client.php
+++ uc_client/client.php
@@ -372,8 +372,8 @@ function uc_user_getprotected() {
 }
 
 function uc_get_user($username, $isuid=0) {
-	//$return = call_user_func(UC_API_FUNC, 'user', 'get_user', array('username'=>$username, 'isuid'=>$isuid));
-	//var_dump($return);
+	$return = call_user_func(UC_API_FUNC, 'user', 'get_user', array('username'=>$username, 'isuid'=>$isuid));
+	var_dump($return);
 	//return UC_CONNECT == 'mysql' ? $return : uc_unserialize($return);
 }
 
